89cf58d5d757a8059863e2b963152e00a31006f2
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2023-03-14 10:00:13 +0500

    I have updated the panaverse community section's syllabus.

--- src/components/Panaverse_Community.tsx
+++ src/components/Panaverse_Community.tsx
@@ -1,47 +1,46 @@
 "use client";
-import { Flex, Container, VStack, Box, Heading, Text } from "@chakra-ui/react";
+import {
+  Flex,
+  Container,
+  VStack,
+  Box,
+  Heading,
+  Text,
+  SimpleGrid,
+  Stack,
+} from "@chakra-ui/react";
 import Image from "next/image";
+import Link from "next/link";
 
 export const PanaverseCommunity = () => {
   return (
     <>
-      <Box as="main">
-        <Container maxW="8xl">
-          <Flex
-            flexDir={{ base: "column", md: "row" }}
-            align={{ base: "center", md: "center" }}
-            maxW={"1440px"}
-            mx="auto"
-            pt={{ base: "5", md: "10" }}
-            px={{ base: "10", md: "20" }}
-            textAlign={{ base: "center", md: "center" }}
-            justifyContent={{ base: "center", md: "flex-end" }}
-          >
-            <Box
-              as="section"
-              order={{ base: "2", md: "2" }}
-              mr={{ md: "auto" }}
+      <Container maxW={"5xl"} py={6} fontFamily="sans-serif">
+        <SimpleGrid columns={{ base: 1, md: 1 }} spacing={10}>
+          <Stack spacing={4}>
+            <Heading
+              as="h2"
+              fontSize="1.5em"
+              fontWeight="semibold"
+              color="#159957"
+              fontFamily="serif"
             >
-              <Box
-                display={"flex"}
-                justifyContent={{ base: "center", md: "flex-start" }}
-                mb="4px"
-                fontSize={{ base: "20px", md: "large" }}
-              >
-                <Text fontWeight={"bold"}>
-                  The Panaverse Community and Syllabus:
-                </Text>
-              </Box>
-              <Text wordBreak="inherit" fontFamily="sans-serif">
-                The internet is without a doubt the most important technological
-                development in human history. Web3, 3D Metaverse, AI, IoT, Cloud
-                and Edge technologies expand the internet as we know it by
-                introducing novel features and advancements. Metaverse will make
-                use of all aspects of modern technology, including 3D, VR, AR,
-                AI, blockchain, cloud and edge computing, voice computing,
-                ambient computing, and more
-                <br />
-              </Text>
+              The Panaverse Community and Syllabus:
+            </Heading>
+            <Text
+              as="div"
+              color="#606C71"
+              fontSize={"lg"}
+              fontFamily="serif"
+              fontWeight="medium"
+            >
+              The internet is without a doubt the most important technological
+              development in human history. Web3, 3D Metaverse, AI, IoT, Cloud
+              and Edge technologies expand the internet as we know it by
+              introducing novel features and advancements. Metaverse will make
+              use of all aspects of modern technology, including 3D, VR, AR, AI,
+              blockchain, cloud and edge computing, voice computing, ambient
+              computing, and more
               <VStack px="10" py="10">
                 <Box>
                   <Image
@@ -52,18 +51,54 @@ export const PanaverseCommunity = () => {
                   />
                 </Box>
               </VStack>
-              <Text wordBreak="inherit" fontFamily="sans-serif">
-                Citi is the latest Wall Street business to give a positive
-                prognosis for Web 3.0 and the Metaverse, terms used to depict a
-                future internet vision centered on decentralized technologies
-                and virtual worlds. Citi stated in a March 2022 research paper
-                that the metaverse economy might have a total addressable market
-                of up to $13 trillion and five billion people by 2030.
+              Citi is the latest Wall Street business to give a positive
+              prognosis for Web 3.0 and the Metaverse, terms used to depict a
+              future internet vision centered on decentralized technologies and
+              virtual worlds. Citi stated in a March 2022 research paper that
+              the metaverse economy might have a total addressable market of up
+              to $13 trillion and five billion people by 2030.
+              <Text
+                as="span"
+                ml="0.4rem"
+                color="#1e6bb8"
+                bgColor="transparent"
+                fontWeight="normal"
+                _hover={{ cursor: "pointer", textDecoration: "underline" }}
+              >
+                <Link
+                  href="https://www.citivelocity.com/citigps/metaverse-and-money/"
+                  target="blank"
+                >
+                  https://www.citivelocity.com/citigps/metaverse-and-money/
+                </Link>
               </Text>
-            </Box>
-          </Flex>
-        </Container>
-      </Box>
+            </Text>
+
+            <Heading
+              as="h2"
+              fontSize="1.5em"
+              fontWeight="semibold"
+              color="#159957"
+            >
+              The Program in a Nutshell: Earn While You Learn
+            </Heading>
+
+            <Text
+              as="p"
+              fontSize="lg"
+              fontFamily="serif"
+              color="#606C71"
+              fontWeight="medium"
+            >
+              In this brand-new type of curriculum, students will learn how to
+              make money and boost exports in the classroom and will begin doing
+              so within six months of the program's beginning. It resembles a
+              cross between a corporate venture and an educational project.
+            </Text>
+          </Stack>
+        </SimpleGrid>
+      </Container>
+
       <Introduction />
     </>
   );
@@ -72,71 +107,34 @@ export const PanaverseCommunity = () => {
 export function Introduction() {
   return (
     <>
-      <Box as="main">
-        <Container maxW="8xl">
-          <Flex
-            flexDir={{ base: "column", md: "row" }}
-            align={{ base: "center", md: "center" }}
-            maxW={"1440px"}
-            mx="auto"
-            pt={{ base: "5", md: "10" }}
-            px={{ base: "10", md: "20" }}
-            textAlign={{ base: "center", md: "center" }}
-            justifyContent={{ base: "center", md: "flex-end" }}
-          >
-            <Box
-              as="section"
-              order={{ base: "2", md: "2" }}
-              mr={{ md: "auto" }}
+      <Container maxW={"5xl"} py={2} fontFamily="serif">
+        <SimpleGrid columns={{ base: 1, md: 1 }} spacing={10}>
+          <Stack spacing={4}>
+            <Heading
+              as="h2"
+              fontSize="1.5em"
+              fontWeight="semibold"
+              color="#159957"
             >
-              <Box
-                display={"flex"}
-                justifyContent={{ base: "center", md: "flex-start" }}
-                mb="4px"
-                fontSize={{ base: "20px", md: "large" }}
-              >
-                <Text fontWeight={"bold"}>
-                  The Program in a Nutshell: Earn While You Learn
-                </Text>
-              </Box>
-              <Text wordBreak="inherit" fontFamily="sans-serif">
-                In this brand-new type of curriculum, students will learn how to
-                make money and boost exports in the classroom and will begin
-                doing so within six months of the program's beginning. It
-                resembles a cross between a corporate venture and an educational
-                project.
-              </Text>
-
-              <Box
-                display={"flex"}
-                justifyContent={{ base: "center", md: "flex-start" }}
-                mb="4px"
-                fontSize={{ base: "20px", md: "large" }}
-              >
-                <Text fontWeight={"bold"} mt="10px">
-                  Program of Studies
-                </Text>
-              </Box>
-
-              <Text wordBreak="inherit" fontFamily="sans-serif">
-                This curriculum is intended for beginners who want to learn
-                software development from the ground up. The first three
-                quarters are shared by all specialties and are dedicated to
-                studying Object-Oriented Programming and cutting-edge Full-Stack
-                Web 2.0 development. It is going to be a fifteen-month-long
-                hybrid program that includes both onsite and online classes and
-                is divided into five quarters of 13 weeks each. The emphasis
-                will be on hands-on learning by educating students to produce
-                projects. To accommodate everyone, courses will be held
-                primarily on weekends or after 6:00 p.m. (Pakistan Time) on
-                weekdays. It employs a hybrid teaching format, with core onsite
-                classes complemented by online Zoom laboratories and recorded
-                videos.
-              </Text>
-            </Box>
-          </Flex>
-        </Container>
-      </Box>
+              Program of Studies
+            </Heading>
+            <Text color={"gray.500"} fontSize={"lg"} fontFamily="sans-serif">
+              This curriculum is intended for beginners who want to learn
+              software development from the ground up. The first three quarters
+              are shared by all specialties and are dedicated to studying
+              Object-Oriented Programming and cutting-edge Full-Stack Web 2.0
+              development. It is going to be a fifteen-month-long hybrid program
+              that includes both onsite and online classes and is divided into
+              five quarters of 13 weeks each. The emphasis will be on hands-on
+              learning by educating students to produce projects. To accommodate
+              everyone, courses will be held primarily on weekends or after 6:00
+              p.m. (Pakistan Time) on weekdays. It employs a hybrid teaching
+              format, with core onsite classes complemented by online Zoom
+              laboratories and recorded videos.
+            </Text>
+          </Stack>
+        </SimpleGrid>
+      </Container>
     </>
   );
 }
